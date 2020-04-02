109ee886622ff23f4d59534384ec581829728312
JP McGlone <jp@jpmcglone.com>
2020-04-02 17:56:12 -0400

    done

---
+++ src/components/header.js
@@ -0,0 +1,45 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import Image from "../components/image"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `white`
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+      }}
+    >
+      <div style={{ backgroundColor: "#273B80" }}>
+        <div style={{ padding: "10pt 10pt", maxWidth: `150pt` }}>
+          <Image style={{ padding: 0 }} />
+        </div>
+      </div>
+      {/* <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1> */}
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Img from "gatsby-image/withIEPolyfill"
+import SEO from "../components/seo"
+
+
+const Index = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file: file(relativePath: { eq: "swiftguild-square-logo.png" }) {
+        childImageSharp {
+          fixed(width: 500) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      file2: file(relativePath: { eq: "jordan-peterson.png" }) {
+        childImageSharp {
+          fixed(width: 500) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout style={{ textAlign: "center" }}>
+      <SEO title="Home" />
+
+      <div>
+
+        <div style={{ textAlign: "center", marginTop: 50, maxWidth: 1024, marginLeft: "auto", marginRight: "auto" }}>
+          <h4>JP McGlone, LLC<br/>is under construction</h4>
+          <p style={{ fontSize: 14 }}>In the meantime, check out our current projects:</p>
+
+          <div style={{ margin: "0 auto" }}>
+            <a style={{ display: "inline-block" }} href="https://swiftguild.com" target="_blank" alt="Swift Guild">
+              <Img style={{ margin: 5, maxWidth: 100, maxHeight: 100 }} objectFit="cover" objectPosition="50% 80%" alt="Swift Guild" fixed={data.file.childImageSharp.fixed} />
+            </a>
+
+            <a style={{ display: "inline-block" }} href="https://www.meetup.com/Jordan-Peterson-Study-Group/" target="_blank" alt="Jordan Peterson Study Group">
+              <Img style={{ margin: 5, maxWidth: 100, maxHeight: 100 }} objectFit="cover" objectPosition="50% 80%" alt="Jordan Peterson Study Group" fixed={data.file2.childImageSharp.fixed} />
+            </a>
+
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Index
