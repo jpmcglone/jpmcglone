af7fe5a0d320f4eeb1aa029668706c2fa0144f1d
John P. McGlone <jp@jpmcglone.com>
2025-02-05 09:39:28 -0500

    Updates

---
+++ _nuxt/CMw0DzuF.js
@@ -0,0 +1,1 @@
+import{r as t,c as o,a,o as r}from"./B5PQ0mIJ.js";const l={__name:"about",setup(s){return t("About"),(n,e)=>(r(),o("div",null,e[0]||(e[0]=[a("h1",null,"About",-1)])))}};export{l as default};
