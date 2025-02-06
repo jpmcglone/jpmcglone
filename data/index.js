d6986e8489ecf37b3cec53178044480e29639348
John P. McGlone <jp@jpmcglone.com>
2025-02-06 13:39:36 -0500

    Updates

---
+++ data/index.js
@@ -0,0 +1,44 @@
+export default {
+  personalInfo: {
+    username: '@jpmcglone',
+    title: 'Senior Software Engineer',
+    avatar: '/images/johnmcglone.jpg', 
+    description: 'Software Engineer specializing in iOS and Full Stack Development',
+    tags: [
+      {
+        label: 'Software Engineer',
+        tooltip: 'Check out my code on GitHub',
+        link: 'https://github.com/jpmcglone'
+      },
+      {
+        label: 'Husband',
+        tooltip: 'Married to my wonderful wife Katrina',
+        link: 'https://johnandkatmcglone.com/',
+      },
+      {
+        // label: 'Landlord',
+        tooltip: 'Real estate investor and property manager',
+        // link: '/landlord'
+      },
+      {
+        label: 'Gamer',
+        tooltip: 'Watch me stream games live',
+        link: 'https://twitch.tv/jpmcglone'
+      }
+    ]
+  },
+  resume: {
+    buttonText: 'View Resume',
+    link: '/resume'
+  },
+  meta: {
+    title: '@jpmcglone - Software Engineer',
+    description: 'Software Engineer specializing in iOS and Full Stack Development',
+    ogTitle: 'JP McGlone - Software Engineer',
+    ogDescription: 'Software Engineer specializing in iOS and Full Stack Development',
+    ogType: 'website',
+    twitterCard: 'summary',
+    twitterTitle: 'JP McGlone - Software Engineer',
+    twitterDescription: 'Software Engineer specializing in iOS and Full Stack Development'
+  }
+} 
