1b7fb5b6f40de7bc7af816705a9ba86512553c4e
JP McGlone <jp@jpmcglone.com>
2026-09-22 18:31:51 -0400

    Fix Rumble Studio link

--- components/resume/AboutSection.vue
+++ components/resume/AboutSection.vue
@@ -46,7 +46,7 @@ const props = defineProps({
 
 const linkDescriptions = {
   'https://rumble.com': 'Rumble’s video platform for creators and viewers.',
-  'https://rumble.studio': 'Rumble’s livestreaming product for creators.',
+  'https://studio.rumble.com': 'Rumble’s livestreaming product for creators.',
   'https://www.docusign.com':
     'Digital agreement platform centered on e-signature and workflow automation.',
   'https://imgur.com': 'A large image-sharing and internet culture platform.',
